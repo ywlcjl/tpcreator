03f2cf502f13ff3e22f166e5d044433eb76c6e9b
ywl <ywlcjl2@163.com>
2017-04-05 16:57:10 +0800

    add static

---
+++ public/static/admin/js/public.js
@@ -0,0 +1,153 @@
+/* 后台路径 ~cc */
+var MODULE = 'admin';
+var MODULE_URL = "/" + MODULE + "/";
+
+function confirmAction() {
+    if (confirm("确定该操作吗?")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkAll() {
+    $('#checkAll').click(function () {
+        if ($(this).prop("checked") == true) {
+            $('input[name="ids[]"]').prop('checked', 'checked');
+            $('.bd_table_tr').css('background-color', '#FFF1A6');
+            $('.bd_table_tr').attr('class', 'bd_table_tr2');
+        } else {
+            $('input[name="ids[]"]').prop('checked', '');
+            $('.bd_table_tr2').css('background-color', '');
+            $('.bd_table_tr2').attr('class', 'bd_table_tr');
+        }
+    });
+
+    //绑定普通的选中框变色
+    $('input[name="ids[]"]').click(function () {
+        if ($(this).prop("checked") == true) {
+            //选中
+            $(this).parent().parent().parent().css('background-color', '#FFF1A6');
+            $(this).parent().parent().parent().attr('class', 'bd_table_tr2');
+        } else {
+            //关闭
+            $(this).parent().parent().parent().css('background-color', '');
+            $(this).parent().parent().parent().attr('class', 'bd_table_tr');
+        }
+    });
+
+    return false;
+}
+
+function toMessage() {
+    var secondObj = $('#second');
+    var second = secondObj.html();
+    if (second > 0) {
+        secondObj.html(second - 1);
+        setTimeout('toMessage()', 1000);
+    } else {
+        var toUrl = $('#toUrl').attr('href');
+        location = toUrl;
+    }
+}
+
+function bootstrapDate() {
+    $('input[data-provide="datepicker"]').datepicker({
+        format: 'yyyy-mm-dd',
+        language: 'zh-CN',
+    });
+}
+
+function filterForm() {
+    $('#filterFormTitle').click(function() {
+        var filterForm = $('#filterForm');
+
+        if (filterForm.css('display') == 'none') {
+            $(this).attr('class', 'btn btn-default active');
+            filterForm.show();
+        } else {
+            $(this).attr('class', 'btn btn-default');
+            filterForm.hide();
+        }
+        return false;
+    });
+}
+
+function attachInsertEditor(obj) {
+    var imgUrl = $(obj).attr('href');
+    tinymce.activeEditor.insertContent('<img src="' + imgUrl + '" >');
+    return false;
+}
+
+function setCoverPic(obj) {
+    var imgUrl = $(obj).attr('href');
+    $('#input_cover_pic').val(imgUrl);
+    return false;
+}
+
+function deleteAttach(obj) {
+    var result = confirm('确认删除吗?');
+    if (result) {
+        var id = $(obj).attr('href');
+
+        $.post(MODULE_URL + "attach/ajaxDelete", {
+            'id': id
+        }, function (data, textStatus) {
+            var message = data.message;
+            var success = data.success;
+
+            $('#attachMessage').html(message);
+
+            if (success > 0) {
+                $(obj).parent().remove();
+            }
+        }, "json");
+    }
+
+    return false;
+}
+
+function articleUpload() {
+    $('#buttonUpload').click(function () {
+        var formData;
+        formData = new FormData();
+        formData.append('post', 1);
+        formData.append('image', $("#inputFile").get(0).files[0]);
+
+        $.ajax({
+            url: MODULE_URL+'attach/ajaxUpload',
+            data: formData,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            dataType: 'json',
+            success: function (data) {
+                if (data.success) {
+                    $("#inputFile").val("");
+                    $("#attachMessage").html("");
+
+                    var str = '<li class="list-group-item">';
+                    str += '<input type="hidden" name="attachId[]" value="' + data.attachId + '">';
+                    str += '<a href="/' + data.picUrl + '" target="_blank" class="bd_attach_img"><img src="/' + data.picUrlThumb + '" class="img-thumbnail" alt="' + data.orig_name + '"></a> ';
+                    str += '<a href="/' + data.picUrl + '" onclick="return attachInsertEditor(this)">插入文章</a>&nbsp;&nbsp;';
+                    str += '<a href="' + data.picUrl + '" onclick="return setCoverPic(this);">设为封面</a>&nbsp;&nbsp;';
+                    str += '<a href="/' + data.picUrlThumb + '" onclick="return attachInsertEditor(this)">插入缩略图</a>&nbsp;&nbsp;';
+                    str += '<a href="' + data.picUrlThumb + '" onclick="return setCoverPic(this);">封面缩略图</a>&nbsp;&nbsp;';
+                    str += '<a href="' + data.attachId + '" onclick="return deleteAttach(this);">删除</a>';
+                    str += '</li>';
+
+                    $("#attachList").append(str);
+
+                } else {
+                    $("#attachMessage").html(data.message);
+                }
+
+            },
+            error: function (data) {
+                $("#attachMessage").html("ajax error");
+            }
+        });
+
+        return false;
+    });
+}
